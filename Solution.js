4e9eccb2d30cfd3e2ed1da57027a05b965579c18
Lachezar Kutsarov <lkutsarov@gmail.com>
2026-05-17 16:01:59 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,26 @@
+
+/**
+ * @param {number[]} skills
+ * @param {number} targetNumberOfConsequtiveWins
+ * @return {number}
+ */
+var findWinningPlayer = function (skills, targetNumberOfConsequtiveWins) {
+    let numberOfWins = 0;
+    let indexMaxValue = 0;
+    let maxValue = skills[0];
+    if (targetNumberOfConsequtiveWins > skills.length) {
+        targetNumberOfConsequtiveWins = skills.length;
+    }
+
+    for (let i = 1; i < skills.length && numberOfWins < targetNumberOfConsequtiveWins; ++i) {
+        if (maxValue < skills[i]) {
+            maxValue = skills[i];
+            indexMaxValue = i;
+            numberOfWins = 1;
+        } else {
+            ++numberOfWins;
+        }
+    }
+
+    return indexMaxValue;
+};
